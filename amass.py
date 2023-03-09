7732baa2e7023cb8ddd0928449d2507a1ebc40d0
mark39107pro <102141835+mark39107pro@users.noreply.github.com>
2023-03-09 21:24:46 +0000

    Update amass.py

--- amass.py
+++ amass.py
@@ -1,10 +1,10 @@
 import time
 import os
-with open("d.txt", "r") as a_file:
+with open("d.txt", "r") as a_file:          ##domains.txt will contain domains 
 
   for line in a_file:
 
     domain = line.strip()
-    cmd = ('amass enum -d ' + domain+' -config config.ini  | grep '+domain)
-    os.system(cmd)
-    time.sleep(120)
+    cmd = ('amass enum -d ' + domain+' -config config.ini  | grep '+domain)    ## config.ini and domains.txt file must in same path/directory
+    os.system(cmd)         
+    time.sleep(120)       ## time limit to stop a scan and loop to another target domain in the file
