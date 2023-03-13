d9c73e79c82076a536cfa3d098e213b661e61d45
Aman <102141835+mark39107pro@users.noreply.github.com>
2023-03-13 18:15:52 +0000

    Add files via upload

---
+++ live.py
@@ -0,0 +1,12 @@
+import os
+print('no need for writing full ip:  valid: 1 to infinite')
+n=input("Enter ip no  ")
+cmd = ('sudo python -m http.server '+n)
+root = 'xfce4-terminal'
+exi = 'exit'
+cmd1 =('firefox http://0.0.0.0:'+n)
+os.system(root)
+os.system(cmd1)
+os.system(exi)
+os.system(cmd)
+print('HELLowdwndlqndlkan')
